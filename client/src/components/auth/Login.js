d2bfdefd1b534b9cb53bcc3214222ecc3530a17b
duedeu36 <duedeu36@gmail.com>
2019-04-17 04:37:40 +0200

    frontend: created login/register components, linking input to the register component state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,7 +4,27 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <form>
+          <label>
+            <p ref="animation" className="label-txt">
+              ENTER YOUR EMAIL
+            </p>
+            <input type="text" className="input" />
+            <div className="line-box">
+              <div className="line" />
+            </div>
+          </label>
+          <label>
+            <p ref="animation" className="label-txt">
+              ENTER YOUR PASSWORD
+            </p>
+            <input type="text" className="input" />
+            <div className="line-box">
+              <div className="line" />
+            </div>
+          </label>
+          <button type="submit">login</button>
+        </form>
       </div>
     );
   }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,101 @@
 import React, { Component } from "react";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  //   state = {
+  //     addAnimation: false,
+  //     removeAnimation: false
+  //   };
+
+  //   showAnim = () => {
+  //     this.setState({
+  //       addAnimation: !this.state.addAnimation
+  //     });
+  //   };
+
   render() {
     return (
       <div>
-        <h1>Register</h1>
+        <form>
+          <label>
+            <p ref="animation" className="label-txt">
+              ENTER YOUR EMAIL
+            </p>
+            <input
+              type="text"
+              className="input"
+              defaultValue={this.state.email}
+              onChange={this.onChange}
+              name="email"
+            />
+            <div className="line-box">
+              <div className="line" />
+            </div>
+          </label>
+          <label>
+            <p ref="animation" className="label-txt">
+              ENTER YOUR NAME
+            </p>
+            <input
+              type="text"
+              className="input"
+              defaultValue={this.state.name}
+              onChange={this.onChange}
+              name="name"
+            />
+            <div className="line-box">
+              <div className="line" />
+            </div>
+          </label>
+          <label>
+            <p ref="animation" className="label-txt">
+              ENTER YOUR PASSWORD
+            </p>
+            <input
+              type="text"
+              className="input"
+              defaultValue={this.state.password}
+              onChange={this.onChange}
+              name="password"
+            />
+            <div className="line-box">
+              <div className="line" />
+            </div>
+          </label>
+          <label>
+            <p ref="animation" className="label-txt">
+              CONFIRM YOUR PASSWORD
+            </p>
+            <input
+              type="text"
+              className="input"
+              defaultValue={this.state.password2}
+              onChange={this.onChange}
+              name="password2"
+            />
+            <div className="line-box">
+              <div className="line" />
+            </div>
+          </label>
+          <button type="submit">register</button>
+        </form>
       </div>
     );
   }
