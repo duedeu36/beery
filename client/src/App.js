e8177d5fa00bd20f0a909390d65fde4f6be1c568
duedeu36 <duedeu36@gmail.com>
2019-04-25 21:56:45 +0200

    redux: added profileReducer and actions for profile

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -12,6 +13,7 @@ import Navbar from "./components/layout/Navbar";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Home from "./components/layout/Home";
+import Beerwall from "./components/beerwall/Beerwall";
 
 import "./App.css";
 
@@ -27,9 +29,10 @@ if (localStorage.jwtToken) {
   // Check for expired token
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
-    //   Logout user
+    // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current profile
+    // Clear current profile
+    store.dispatch(clearCurrentProfile);
     // Redicret to login
     window.location.href = "./login";
   }
@@ -41,12 +44,14 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div className="App">
-            <div className="container" />
+            <div className="container">
+              <Route exact path="/" component={Landing} />
+            </div>
             <Navbar />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/" component={Landing} />
             <Route exact path="/home" component={Home} />
+            <Route exact path="/beerwall" component={Beerwall} />
             <Footer />
           </div>
         </Router>
